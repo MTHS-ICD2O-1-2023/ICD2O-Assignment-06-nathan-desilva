1a53d22ae4fe04e596e7a9a9da066f421454ff75
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-06-16 21:08:47 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -6,34 +6,29 @@
 
 "use strict"
 
+/**
+ * Check service worker.
+ */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICD2O-Unit-6-03-nathan-desilva/sw.js", {
-    scope: "/ICD2O-Unit-6-03-nathan-desilva/sw.js",
+  navigator.serviceWorker.register("/ICS2O-Assignment-06-nathan-desilva/sw.js", {
+    scope: "/ICD2O-Assignment-06-nathan-desilva",
   })
 }
 
 /**
- * Get API info.
+ * This function displays random dad jokes.
  */
-// code from: https://www.youtube.com/watch?v=670f71LTWpM
-
-const getWeather = async (URLAddress) => {
+const getDuck = async (URLAddress) => {
   try {
     const result = await fetch(URLAddress)
-    const jsonData = await result.json()
-    console.log(jsonData)
-    // Get weather and convert from kelvin to celsius
-    const weather = Math.round(jsonData.main.temp - 273.15)
-    document.getElementById("api-weather").innerHTML = "<p>Temperature: " + weather + "°C</p>"
-    // Get weather icon
-    const icon = jsonData.weather[0].icon
-    const iconURL = "http://openweathermap.org/img/w/" + icon + ".png"
-    document.getElementById("api-icon").innerHTML = "<img src='" + iconURL + "' class='donut'></img>"
+    const data = await result.json()
+    console.log(data)
+    document.getElementById("joke").innerHTML = data.setup + "<br><br>" + data.punchline
   } catch (err) {
     console.log(err)
   }
 }
 
-getWeather(
-  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+getDuck(
+  "https://random-d.uk/api/images/61.jpg"
 )
